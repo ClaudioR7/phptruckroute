1bac52c9ead31550f9312d41ea2d7a6a0711acb7
Claudio Rocha Bastos Freire <claudio7772018@outlook.com>
2026-06-09 17:49:30 -0300

    alteração do index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/db.php';
 
+echo "<pre>";
+var_dump(getenv('MYSQLHOST'), getenv('MYSQLPORT'), getenv('MYSQLDATABASE'), getenv('MYSQLUSER'));
+echo "</pre>";
+exit;
+
 if (!empty($_SESSION['usuario_id'])) {
     header('Location: ' . APP_URL . '/pages/dashboard.php'); exit;
 }
